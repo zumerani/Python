e79bd307c151586246119fddcabc9744c20efbdb
Zain Umerani <zumerani@gmail.com>
2016-12-25 19:14:09 -0800

    Played with loops and making code shorter

--- Loops and Conditions/loopsAndConditions.py
+++ Loops and Conditions/loopsAndConditions.py
@@ -8,6 +8,21 @@ my_list = [1 , 3 , 5 , 7 , 9]
 for i in my_list:
     print( i ** 2 ) #i to the power of 2
 
+#Ranges
+myList = [ x for x in range(5) ] #generates: [0, 1 , 2 , 3 , 4]
+
+print("myList: {}".format(myList) )
+
+myMutlipleList = [ x * 3 for x in range(3) ] #generates: [0 , 3 , 6]
+print("myMultipleList: {}".format( myMutlipleList) )
+
+myModulo = [ x for x in range(10) if x % 2 == 0 ] #generates: [0 , 2 , 4 , 6 , 8]
+print( "myModulo is: {}".format( myModulo ) )
+
+#Playing with strings and lists
+myPeople = ["Zain" , "ALBERT" , "  Barry" , "Ken" ]
+fixedMyPeople = [ person.strip().lower() for person in myPeople ]#generates: ['zain', 'albert', 'barry', 'ken']
+print( "fixedMyPeople is: {}".format( fixedMyPeople ) )
 
 #Boolean
 myBoolean = True
@@ -34,4 +49,5 @@ if userInput in people:
     print( "We found the person: {}".format(userInput) ) #Formatting strings appropriately
 else: #don't forget the colon
     print("You do not know this person")
+
 
--- Loops and Conditions/whoDoYouKnow.py
+++ Loops and Conditions/whoDoYouKnow.py
@@ -1,11 +1,18 @@
 def whoDoYouKnow():
-    people = input("Enter people you know separatd by commas:")
-    knownList = people.split(",")
-    noSpaces = []
-    for i in knownList:
-        noSpaces.append( i.strip() ) #Using 'strip' to take out extra white spaces incase user inputs with spaces
+    people = input("Enter people you know separated by commas:")
+    #knownList = people.split(",")
+    #noSpaces = []
+    #for i in knownList:
+    #    noSpaces.append( i.strip() ) #Using 'strip' to take out extra white spaces incase user inputs with spaces
+
+    #Rather than doing the above code^, we can do this:
+    noSpaces= [ person.strip() for person in people.split(",") ] #grabs the list with ',' delimeter and strips each word
+    print("noSpaces is: {}".format(noSpaces))
 
     return noSpaces
+
+    #To make the function a one-liner, we can also do this:
+    #return [ person.strip() for person in input("Enter people you know separated by commas: ").split(",") ]
 
 def askUser():
     list = whoDoYouKnow()
